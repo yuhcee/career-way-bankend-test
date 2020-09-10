7c4e6d7344eb99202dc63e8e0315d0e1a7444d5d
Uchenna Egbo <egbou@accessbankplc.com>
2020-09-10 09:53:05 +0100

    bootstrap proj

--- src/index.js
+++ src/index.js
@@ -1,10 +1,19 @@
-require('dotenv').config();
+import server, { socketServer } from './server';
 
-import server from './server';
+import dotenv from 'dotenv';
+dotenv.config();
+const PORT = process.env.PORT || 3300;
 
-const port = process.env.PORT || 3000;
+const { log } = console;
 
-server.server.listen(port, () => {
-  const { log } = console;
-  log(`*** server running on port ${port} ***`);
+// ping all external api before starting the server;
+
+server.server.listen(PORT, function () {
+  const { address, port } = this.address();
+  const server = `http://${address === '::' ? '0.0.0.0' : address}:${port}`;
+  log('\x1b[1m\x1b[31m Server Started ON:', '\x1b[36m\x1b[89m\x1b[4m', server, '\x1b[0m');
+  log('\x1b[1m\x1b[31m', 'Press Ctrl+C to quit.\n\x1b[0m');
 });
+
+
+export default { app: server, socketServer };
