f1bae8163d55aef1ef95d39a21bf5c197a7795cf
yuhcee <egbouchenna1@gmail.com>
2021-03-29 00:33:13 +0100

    rename server to app.js

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import server from './server';
+import app from './app';
 
 import dotenv from 'dotenv';
 dotenv.config();
@@ -8,17 +8,16 @@ const { log } = console;
 // const { server: app } = server;
 
 // ping all external api before starting the server;
-server.listen(PORT, function () {
-  const server = `http://localhost:${PORT}`;
-  console.log('App started on port ' + server);
-});
-// server.listen(PORT, function () {
+app.listen(PORT);
+const server = `http://localhost:${PORT}`;
+log('Server started on port ' + server);
+// app.listen(PORT, function () {
 //   const { address, port } = this.address();
-//   const server = `http://${address === '::' ? '0.0.0.0' : address}:${port}`;
+//   const app = `http://${address === '::' ? '0.0.0.0' : address}:${port}`;
 //   log(
-//     '\x1b[1m\x1b[31m Server Started ON:',
+//     '\x1b[1m\x1b[31m app Started ON:',
 //     '\x1b[36m\x1b[89m\x1b[4m',
-//     server,
+//     app,
 //     '\x1b[0m'
 //   );
 //   log('\x1b[1m\x1b[31m', 'Press Ctrl+C to quit.\n\x1b[0m');
