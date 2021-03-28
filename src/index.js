6bf5e3a8eb871dce1bd8a4ee47939bddaa8ce45b
yuhcee <egbouchenna1@gmail.com>
2021-03-28 22:31:50 +0100

    update v.4

--- src/index.js
+++ src/index.js
@@ -9,15 +9,19 @@ const { log } = console;
 
 // ping all external api before starting the server;
 server.listen(PORT, function () {
-  const { address, port } = this.address();
-  const server = `http://${address === '::' ? '0.0.0.0' : address}:${port}`;
-  log(
-    '\x1b[1m\x1b[31m Server Started ON:',
-    '\x1b[36m\x1b[89m\x1b[4m',
-    server,
-    '\x1b[0m'
-  );
-  log('\x1b[1m\x1b[31m', 'Press Ctrl+C to quit.\n\x1b[0m');
+  const server = `http://localhost:${PORT}`;
+  console.log('App started on port ' + server);
 });
+// server.listen(PORT, function () {
+//   const { address, port } = this.address();
+//   const server = `http://${address === '::' ? '0.0.0.0' : address}:${port}`;
+//   log(
+//     '\x1b[1m\x1b[31m Server Started ON:',
+//     '\x1b[36m\x1b[89m\x1b[4m',
+//     server,
+//     '\x1b[0m'
+//   );
+//   log('\x1b[1m\x1b[31m', 'Press Ctrl+C to quit.\n\x1b[0m');
+// });
 
 // export default app;
